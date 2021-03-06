4ea0eec3c88d3fbb91bdef168d39552a514a090f
Vinh <vinh.giang@pushlishone.nl>
2021-03-06 17:32:37 +0700

    Update source

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 
 import './App.css';
-import ColorBoard from './components/color-board/color-board.component';
+import Spinner from './components/spinner/spinner.component';
+
+const ColorBoard = lazy(() =>
+  import('./components/color-board/color-board.component')
+);
 
 const App = () => {
   return (
-    <ColorBoard/>
+    <Suspense fallback={<Spinner />}>
+      <ColorBoard />
+    </Suspense>
   );
 };
 
